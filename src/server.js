b547895f70f44f56f38ba51c2003cbf63649d312
Brandy <Bengc102@gmail.com>
2026-01-18 20:56:44 +0100

    PORT removed

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ import db from "./db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.set("trust proxy", 1);
