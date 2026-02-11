abf2963015d19d21765cac93158d0d1f3130a9da
Brandy <Bengc102@gmail.com>
2026-02-11 20:22:04 +0100

    currency adds

--- src/routes/homeListings.js
+++ src/routes/homeListings.js
@@ -4,15 +4,23 @@ import db from "../db.js";
 
 // Route to get home listings
 router.get("/listings", (req, res) => {
-  db.query(
-    "SELECT * FROM userlistings ORDER BY random() LIMIT 10;",
-    (err, result) => {
-      if (err) {
-        console.error("Error fetching home listings:", err);
-        return res.status(500).json({ error: "Internal server error" });
-      }
-      res.status(200).json(result.rows);
-    },
-  );
+  const query = `
+    SELECT l.*, i.imageurl 
+    FROM userlistings l
+    LEFT JOIN (
+      SELECT DISTINCT ON (listingid) listingid, imageurl 
+      FROM listing_images
+    ) i ON l.id = i.listingid
+    ORDER BY random() 
+    LIMIT 10;
+  `;
+
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error("Error fetching home listings:", err);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+    res.status(200).json(result.rows);
+  });
 });
 export default router;
